f4ff1b4a246632a589ed4a5762970923359904ac
hideaki10 <senshikou1985@gmail.com>
2025-08-11 16:07:07 +0900

    add router package

--- main.go
+++ main.go
@@ -4,18 +4,30 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/go-chi/chi/v5"
 	"github.com/hideaki10/apiserver/handlers"
 )
 
 func main() {
 
-	http.HandleFunc("/hello", handlers.HelloHandler)
-	http.HandleFunc("/article", handlers.PostArticleHandler)
-	http.HandleFunc("/article/list", handlers.ArticlelistHandler)
-	http.HandleFunc("/article/l", handlers.ArticleDetailHandler)
-	http.HandleFunc("/article/nice", handlers.PostNiceHandler)
-	http.HandleFunc("/comment", handlers.CommentHandler)
+	r := chi.NewRouter()
+
+	r.Get("/hello", handlers.HelloHandler)
+	r.Get("/article", handlers.PostArticleHandler)
+	r.Get("/article/list", handlers.ArticlelistHandler)
+
+	r.Get("/article/l", handlers.ArticleDetailHandler)
+	r.Get("/article/nice", handlers.PostNiceHandler)
+	r.Get("/article/nice", handlers.PostNiceHandler)
+	r.Post("/comment", handlers.CommentHandler)
+
+	// http.HandleFunc("/hello", handlers.HelloHandler)
+	// http.HandleFunc("/article", handlers.PostArticleHandler)
+	// http.HandleFunc("/article/list", handlers.ArticlelistHandler)
+	// http.HandleFunc("/article/l", handlers.ArticleDetailHandler)
+	// http.HandleFunc("/article/nice", handlers.PostNiceHandler)
+	// http.HandleFunc("/comment", handlers.CommentHandler)
 
 	log.Println("server start at port 8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
